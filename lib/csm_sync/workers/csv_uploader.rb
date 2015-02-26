076867d1b8abbefc8aa051fd3e23684f2bc61b48
Adam Crownoble <adam.crownoble@biola.edu>
2015-02-26 11:20:26 -0800

    Implement CSV upload feature

--- lib/csm_sync/workers/csv_uploader.rb
+++ lib/csm_sync/workers/csv_uploader.rb
@@ -1,20 +1,19 @@
 module CSMSync
   module Worker
-    class UploadError < StandardError; end
-
     class CSVUploader
       include Sidekiq::Worker
 
+      FILE_NAME = 'csm_export.csv'
+      TIMEOUT = 60 * 5 # 5 min
+
       def perform(file_path)
-        # TODO
+        # A bad password triggers a password prompt that will just sit there. So we need to trigger a timeout if it goes too long.
+        Timeout.timeout(TIMEOUT) do
+          # Net::SCP seems to just return nil on sucess and always raise an erorr on failure. So not much to check here.
+          Net::SCP.upload!(Settings.upload.host, Settings.upload.username, file_path, FILE_NAME, ssh: {password: Settings.upload.password})
+        end
 
-        # raise UploadError, "#{}" unless successful?(nil)
-      end
-
-      private
-
-      def successful?(response)
-        # TODO
+        File.unlink file_path
       end
     end
   end
